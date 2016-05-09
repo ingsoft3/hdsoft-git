51494ee0cf2d561e635eadf2ae222988ef55151f
pipe <hd.soft2016@gmail.com>
2016-05-09 18:07:48 -0500

    Creación Bean Login

---
+++ hdsoft/src/java/BeanSesion/MbSLogin.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BeanSesion;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Felipe
+ */
+@ManagedBean
+@SessionScoped
+public class MbSLogin {
+
+    /**
+     * Creates a new instance of MbSLogin
+     */
+    public MbSLogin() {
+    }
+    
+}
